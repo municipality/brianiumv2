2e826ea6fe605619bdbf321482178db38ea61ca0
Brian Lee <brian.lee@servicemax.com>
2016-11-16 16:54:04 -0800

    typings.json changes

---
+++ app/components/homepage/title.ts
@@ -0,0 +1,24 @@
+import {Component} from '@angular/core';
+
+@Component ({
+    selector : 'title-text',
+    template : `
+        <div>
+
+        
+    `
+})
+
+export class TitleText {
+
+}
+
+@Component ({
+    selector : 'title-container',
+    directives : [],
+    template : ``
+})
+
+export class Title {
+
+}
